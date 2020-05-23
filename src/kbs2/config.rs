9cef00707327ddeff0acafda777219a8a182c491
William Woodruff <william@yossarian.net>
2020-05-23 00:54:04 -0400

    kbs2/config: Add a doc line for UNWRAPPED_KEY_SHM_NAME

--- src/kbs2/config.rs
+++ src/kbs2/config.rs
@@ -32,6 +32,7 @@ pub static CONFIG_BASENAME: &str = "kbs2.conf";
 // the configuration directory.
 pub static DEFAULT_KEY_BASENAME: &str = "key";
 
+// The name for the POSIX shared memory object in which the unwrapped key is stored.
 pub static UNWRAPPED_KEY_SHM_NAME: &str = "__kbs2_unwrapped_key";
 
 // The default base directory name for the secret store, placed relative to
