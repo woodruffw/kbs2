587e04ee25fa4cbe0d0fc2d12f477f8f57ec7b8d
William Woodruff <william@yossarian.net>
2020-06-09 00:02:39 -0400

    kbs2: Add session unit tests
    See #10.

--- src/kbs2/session.rs
+++ src/kbs2/session.rs
@@ -95,3 +95,169 @@ impl Session {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::{tempdir, TempDir};
+
+    // NOTE: We pass store in here instead of creating it for lifetime reasons:
+    // the temp dir is destroyed when its TempDir object is destructed, so we need
+    // to keep it alive long enough for each unit test.
+    fn dummy_session(store: &TempDir) -> Session {
+        let backend = {
+            let key = age::SecretKey::generate();
+
+            Box::new(backend::RageLib {
+                pubkey: key.to_public(),
+                identities: vec![key.into()],
+            })
+        };
+
+        let config = {
+            config::Config {
+                age_backend: BackendKind::RageLib,
+                // NOTE: We create the backend above manually, so the public_key and keyfile
+                // here are dummy values that shouldn't need to be interacted with.
+                public_key: "not a real public key".into(),
+                keyfile: "not a real private key file".into(),
+                wrapped: false,
+                store: store.path().to_str().unwrap().into(),
+                pre_hook: None,
+                post_hook: None,
+                reentrant_hooks: false,
+                generators: vec![config::GeneratorConfig::Internal(Default::default())],
+                commands: Default::default(),
+            }
+        };
+
+        Session {
+            backend: backend,
+            config: config,
+        }
+    }
+
+    // TODO: Figure out how to test Session::new. Doing so will require an interface for
+    // creating + initializing a config that doesn't unconditionally put the store directory
+    // within the user's data directory.
+
+    #[test]
+    fn test_record_labels() {
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            assert_eq!(session.record_labels().unwrap(), Vec::<String>::new());
+        }
+
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+            let record = record::Record::login("foo", "bar", "baz");
+
+            session.add_record(&record).unwrap();
+            assert_eq!(session.record_labels().unwrap(), vec!["foo"]);
+        }
+    }
+
+    #[test]
+    fn test_has_record() {
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+            let record = record::Record::login("foo", "bar", "baz");
+
+            session.add_record(&record).unwrap();
+            assert!(session.has_record("foo"));
+        }
+
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            assert!(!session.has_record("does-not-exist"));
+        }
+    }
+
+    #[test]
+    fn test_get_record() {
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+            let record = record::Record::login("foo", "bar", "baz");
+
+            session.add_record(&record).unwrap();
+
+            let retrieved_record = session.get_record("foo").unwrap();
+
+            assert_eq!(record, retrieved_record);
+        }
+
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            let err = session.get_record("foo").unwrap_err();
+            assert_eq!(err.to_string(), "no such record: foo");
+        }
+    }
+
+    #[test]
+    fn test_add_record() {
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            let record1 = record::Record::login("foo", "bar", "baz");
+            session.add_record(&record1).unwrap();
+
+            let record2 = record::Record::login("a", "b", "c");
+            session.add_record(&record2).unwrap();
+
+            assert_eq!(session.record_labels().unwrap(), vec!["foo", "a"]);
+
+            // Overwrite foo; still only two records.
+            let record3 = record::Record::login("foo", "quux", "zap");
+            session.add_record(&record3).unwrap();
+
+            assert_eq!(session.record_labels().unwrap(), vec!["foo", "a"]);
+        }
+    }
+
+    #[test]
+    fn test_delete_record() {
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+            let record = record::Record::login("foo", "bar", "baz");
+
+            session.add_record(&record).unwrap();
+
+            assert!(session.delete_record("foo").is_ok());
+            assert!(!session.has_record("foo"));
+            assert_eq!(session.record_labels().unwrap(), Vec::<String>::new());
+        }
+
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            let record1 = record::Record::login("foo", "bar", "baz");
+            session.add_record(&record1).unwrap();
+
+            let record2 = record::Record::login("a", "b", "c");
+            session.add_record(&record2).unwrap();
+
+            assert!(session.delete_record("foo").is_ok());
+            assert_eq!(session.record_labels().unwrap(), vec!["a"]);
+        }
+
+        {
+            let store = tempdir().unwrap();
+            let session = dummy_session(&store);
+
+            let err = session.delete_record("does-not-exist").unwrap_err();
+            assert_eq!(err.to_string(), "no such record: does-not-exist");
+        }
+    }
+}
